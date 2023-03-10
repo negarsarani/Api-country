f3b191d42ae97b95717af25dac469914181b5016
Negar Sarani <negar.sarani2000@gmail.com>
2023-03-10 10:02:06 +0330

    create Html $ css

---
+++ Api-project/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./main.js', './index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      lobster: ['lobster'],
+      Comfortaa: ['comfortaa'],
+    },
+  },
+  plugins: [],
+};
